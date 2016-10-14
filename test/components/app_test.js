dc6ed736c215678042db2ed592ad4673289a1b31
Matjaxon <MattRJackson1@gmail.com>
2016-10-13 21:50:27 -0700

    Initial testing setup

--- test/components/app_test.js
+++ test/components/app_test.js
@@ -1,14 +1,24 @@
-import { renderComponent , expect } from '../test_helper';
+import { renderComponent, expect } from '../test_helper';
 import App from '../../src/components/app';
 
-describe('App' , () => {
-  let component;
+/*
+describe - use to group together similar tests
 
-  beforeEach(() => {
-    component = renderComponent(App);
-  });
+it - test a single attribute of a target
 
-  it('renders something', () => {
-    expect(component).to.exist;
+expect - make an assertion about the target (the component)
+
+Wrapping all of the tests in callbacks lets the testing suite load
+regardless of what errors are in the code rather than crashing everything.
+*/
+
+// Name top level describe just the component being tested
+describe('App', () => {
+  it('shows the correct text', () => {
+
+    // create instance of Ap
+    const component = renderComponent(App);
+
+    expect(component).to.contain('React simple starter');
   });
 });
